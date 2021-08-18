eacdb5cc51605812742008bbede8889ced16927f
wdfky <1417052976@qq.com>
2021-08-18 17:16:04 +0800

    2

--- client.go
+++ client.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"net"
+	"reflect"
 )
 
 type Client struct {
@@ -33,4 +35,40 @@ func NewClient(conn net.Conn) *Client {
 // 	}
 // 	reply = RetRpcData.Reply
 // }
-func (c *Client) Call(name string)
+func (c *Client) Call(name string, fun interface{}) {
+	fn := reflect.ValueOf(fun).Elem()
+	f := func(args []reflect.Value) (results []reflect.Value) {
+		inArgs := make([]interface{}, 0, len(args))
+		for _, v := range args {
+			inArgs = append(inArgs, v.Interface())
+		}
+		rpcReqData := RpcData{
+			Name: name,
+			Args: inArgs,
+		}
+		reqdata, err := EnCode(rpcReqData)
+		if err != nil {
+			return
+		}
+		_, err = c.Conn.Write(reqdata)
+		if err != nil {
+			return
+		}
+		reader := bufio.NewReader(c.Conn)
+		rpcRetData, err := DeCode(reader)
+		if err != nil {
+			return
+		}
+		outArgs := make([]reflect.Value, 0, len(rpcRetData.Args))
+		for i, v := range rpcRetData.Args {
+			if v == nil {
+				outArgs = append(outArgs, reflect.Zero(fn.Type().Out(i)))
+				continue
+			}
+			outArgs = append(outArgs, reflect.ValueOf(v))
+		}
+		return outArgs
+	}
+	v := reflect.MakeFunc(fn.Type(), f)
+	fn.Set(v)
+}
